56a70456b223580e02691684f46bd60db2bf2961
Abhishek kumar satyam <abhishek.satyam96@gmail.com>
2021-03-05 21:53:32 +0530

    initial commit including footer and header

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                Created as a assigment for &nbsp;
+                <a href="https://www.brightmoney.co/" target="_blank" rel="noreferrer">
+                    Bright Money
+                </a>
+            </div>
+        )
+    }
+}
---
+++ src/redux/actions/ActionTypes.js
@@ -0,0 +1,5 @@
+export const ACTION_TYPES = {
+    ADD_BILL: 'add-bill',
+    DELETE_BILL: 'delete-bill',
+    EDIT_BILL: 'edit-bill',
+  };
